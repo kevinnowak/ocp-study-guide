43e3456a78c83066600d73411ba796da1aa3eaf5
Kevin Nowak <kevin.nowak@sollers.eu>
2024-02-15 13:02:40 +0100

    Update

---
+++ src/main/java/com/github/kevinnowak/chp01_building_blocks/Conflicts.java
@@ -0,0 +1,13 @@
+package com.github.kevinnowak.chp01_building_blocks;
+
+import java.util.Date;
+//import java.sql.Date;
+import java.sql.*;
+
+class Conflicts {
+
+    Date date;
+    SQLData sqlData;
+//    java.util.Date date;
+//    java.sql.Date sqlDate;
+}
