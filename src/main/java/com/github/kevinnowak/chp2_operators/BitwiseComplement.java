460b7ebf4b858ff48d9493e125e150f58a46266b
Kevin Nowak <kevin.nowak@sollers.eu>
2024-02-19 16:52:58 +0100

    Update

---
+++ src/main/java/com/github/kevinnowak/chp2_operators/BitwiseComplement.java
@@ -0,0 +1,17 @@
+package com.github.kevinnowak.chp2_operators;
+
+class BitwiseComplement {
+
+    public static void main(String[] args) {
+        int value = 3;
+        int complement = ~value;
+        System.out.println(value);
+        System.out.println(complement);
+        double d = 1.0;
+        System.out.println(--d);
+        System.out.println(11.0 % 3);
+        short x = 10;
+        short y = 3;
+        var z = x * y;
+    }
+}
